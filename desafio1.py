684ef7ceca6aaa0dfc335ce6ad1a1f4f54de2cb4
mcirielli <mcirielli@uade.edu.ar>
2024-08-21 17:33:22 -0300

    Update and rename funciones.py to desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,42 @@
+import random
+
+estudiantes = [
+    "Estudiante1", "Estudiante2", "Estudiante3"
+    ]
+materias = [
+    "Materia1", "Materia2", "Materia3"
+    ]
+
+e = len(estudiantes)
+m = len(materias)
+matriz = []
+
+for i in range(e):
+    fila = [0] * m
+    for j in range(m):
+        fila[j] = random.randint(1, 10)
+    matriz += [fila]
+
+def mostrar_matriz(matriz, estudiantes, materias):
+    for i in range(m):
+        print(materias[i])
+    for i in range(e):
+        print(estudiantes[i])
+        for j in range(m):
+            print(matriz[i][j])
+        print()
+
+def calc_prom(matriz):
+    for i in range(e):
+        suma = 0
+        for j in range(m):
+            suma += matriz[i][j]
+        promedio = suma / m
+        print(estudiantes[i])
+        print("Promedio:")
+        print(promedio)
+
+mostrar_matriz(matriz, estudiantes, materias)
+calc_prom(matriz)
+
+    
--- funciones.py
+++
@@ -1,3 +0,0 @@
-def funcion(x):
-    return x
-    
